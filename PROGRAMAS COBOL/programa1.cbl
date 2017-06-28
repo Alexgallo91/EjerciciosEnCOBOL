985dc4f88dc75f963701baa08dc94c8851059d00
Abraham Araujo Moreno <alex_91y@hotmail.com>
2017-06-28 02:46:42 -0500

    Add files via upload

---
+++ PROGRAMAS COBOL/programa1.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGRAMA1.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       0001-INICIO.
+           DISPLAY "HOLA MUNDO"
+           STOP RUN.
